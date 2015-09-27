00360be74ebcd5bf02e38368dd0404de1dd5cea4
Julian Hector <julian.r.hector@gmail.com>
2015-09-27 20:51:22 +0200

    Add helper functions to database class

--- include/Database.php
+++ include/Database.php
@@ -13,5 +13,30 @@ class Database
 		if (!mysql_select_db($db))
 			throw new Exception('Database doesn\'t exist: '.mysql_error());
 	}
+
+	public function select($what, $table, $condition)
+	{
+		if (preg_match('/[^a-zA-Z0-9_]union[^a-zA-Z0-9_]/i', $condition))
+			throw new Exception('oO come on, stop being silly...');
+
+		$qry = "SELECT $what FROM {$this->prefix}{$table} $condition";
+		$result = mysql_query($qry);
+
+		if (!$result)
+			throw new Exception(mysql_error());
+
+		$ret = array();
+		while ($row = mysql_fetch_array($result))
+			array_push($ret, $row);
+
+		return $ret;
+	}
+
+	public function sanitize($input)
+	{
+		$blacklist = array('\'', '"', '/', '*');
+
+		return str_replace($blacklist, '', $input);
+	}
 }
 ?>
